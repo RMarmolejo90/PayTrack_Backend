15c734b096c78af2f86ec81abcac720d934b5294
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-07-11 17:06:37 -0400

    fixed axios header error with if else statement

--- Server.js
+++ Server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require('cors');
+
 // Dependencies
 require('./database/DbConnect.js');
 app.use(cors(
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,8 +1,15 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: '../../../.env' });
 const User = require('../models/Users');
 const bcrypt = require('bcrypt');
 const saltRounds = 5;
+const jwt = require('jsonwebtoken');
 
 const registerUser = async (req, res) => {
+
+  //test dotenv
+  console.log(process.env.TEST);
+
   try {
     const { firstName, lastName, email, password } = req.body;
     const hash = await bcrypt.hash(password, saltRounds);
@@ -11,20 +18,18 @@ const registerUser = async (req, res) => {
     const userExists = await User.exists({ email });
     if (userExists) {
       res.send('User already exists');
+    } else {
+
+      // User does not exist, save the credentials
+      await User.create({
+        firstName,
+        lastName,
+        email,
+        password: hash,
+      });
+      // Registration successful
+      res.send("Success");
     }
-
-    // User does not exist, save the credentials
-    await User.create({
-      firstName,
-      lastName,
-      email,
-      password: hash,
-    });
-
-    // Registration successful, do something else if needed
-    res.send("Success");
-    // ...
-
   } catch (error) {
     console.error(error);
     // res.status(500).send('Problem with saving to the database');
