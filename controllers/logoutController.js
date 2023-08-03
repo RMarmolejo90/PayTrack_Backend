31e32eceb68af7884ca87dfffe5ae94c429d57cf
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-08-02 21:07:17 -0400

    move logout to client

--- controllers/logoutController.js
+++
@@ -1,9 +0,0 @@
-const logout = async () => {
-    try {
-        localStorage.clear();
-        sendStatus(200);
-    } catch(error) {
-        console.error(error);
-    }
-}
-module.exports = logout
