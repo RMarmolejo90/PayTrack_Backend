ce9c30a659f973c72f7d8e8151fbc58ca2ebf7f4
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-08-28 21:04:05 -0400

    fix the header bug

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -5,17 +5,20 @@ const User = require('../models/Users')
 const authenticateToken = async (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
+  const userId = authHeader.userId;
 
   if (token === null) return res.sendStatus(401);
 
   try {
     console.log(authHeader);
     console.log(token);
-    const {_id} = jwt.verify(token, process.env.SECRET_KEY);
-    console.log(_id);
-    const user = await User.findOne({_id});
-    console.log(user);
-    next();
+    const validUser = jwt.verify(token, process.env.SECRET_KEY);
+    console.log(validUser);
+    if (validUser){
+      const user = await User.findOne({userId});
+      console.log(user);
+      next();
+    }
   } catch (error) {
     console.log(error);
     return res.sendStatus(403).json({error: 'request not authorized'}); 
